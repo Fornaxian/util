7b74b1181b98eced2d6bef4e67e37b063ca7d52c
Fornax <wim@fornaxian.tech>
2022-06-03 22:11:40 +0200

    Generic FormatData

--- format_data.go
+++ format_data.go
@@ -2,9 +2,15 @@ package util
 
 import "fmt"
 
+type Number interface {
+	int | int64 | uint64 | float64
+}
+
 // FormatData prints an amount of bytes in a readable rounded amount. The total
 // number of digits before and after the decimal point will always be 3.
-func FormatData(size int64) string {
+func FormatData[T Number](size T) string {
+	var sizef = float64(size)
+
 	var fmtSize = func(n float64, u string) string {
 		var f string
 		if n >= 100 {
@@ -16,20 +22,21 @@ func FormatData(size int64) string {
 		}
 		return fmt.Sprintf(f+" "+u, n)
 	}
+
 	if size >= 1e18 {
 		// An exabyte is the largest volume of data you can express in a signed
 		// 64-bit integer
-		return fmtSize(float64(size)/1e18, "EB")
-	} else if size >= 1e15 {
-		return fmtSize(float64(size)/1e15, "PB")
-	} else if size >= 1e12 {
-		return fmtSize(float64(size)/1e12, "TB")
-	} else if size >= 1e9 {
-		return fmtSize(float64(size)/1e9, "GB")
-	} else if size >= 1e6 {
-		return fmtSize(float64(size)/1e6, "MB")
-	} else if size >= 1e3 {
-		return fmtSize(float64(size)/1e3, "kB")
+		return fmtSize(sizef/1e18, "EB")
+	} else if sizef >= 1e15 {
+		return fmtSize(sizef/1e15, "PB")
+	} else if sizef >= 1e12 {
+		return fmtSize(sizef/1e12, "TB")
+	} else if sizef >= 1e9 {
+		return fmtSize(sizef/1e9, "GB")
+	} else if sizef >= 1e6 {
+		return fmtSize(sizef/1e6, "MB")
+	} else if sizef >= 1e3 {
+		return fmtSize(sizef/1e3, "kB")
 	}
-	return fmt.Sprintf("%d B", size)
+	return fmt.Sprintf("%.0f B", sizef)
 }
