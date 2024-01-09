3b8c1f485f1f9cc86b1e1a1e0b2e5a2bf2b622ff
Fornax <wim@fornaxian.tech>
2024-01-09 18:53:48 +0100

    Add more int conversion methods

--- convert_any.go
+++ convert_any.go
@@ -9,7 +9,7 @@ import (
 // Runes will be treated as int32 and bytes as uint8. Strings will be converted
 // using strings.Atoi. An error will be returned if the passed parameter cannot
 // be converted to an int
-func ConvertToInt(i interface{}) (int, error) {
+func ConvertToInt(i any) (int, error) {
 	switch v := i.(type) {
 	case int:
 		return v, nil
@@ -39,4 +39,32 @@ func ConvertToInt(i interface{}) (int, error) {
 		return strconv.Atoi(v)
 	}
 	return 0, fmt.Errorf("%v is not an int", i)
+}
+
+func ConvertToString(i any) (string, error) {
+	switch v := i.(type) {
+	case string:
+		return v, nil
+	case int:
+		return strconv.Itoa(v), nil
+	case int8:
+		return strconv.FormatInt(int64(v), 10), nil
+	case int16:
+		return strconv.FormatInt(int64(v), 10), nil
+	case int32:
+		return strconv.FormatInt(int64(v), 10), nil
+	case int64:
+		return strconv.FormatInt(int64(v), 10), nil
+	case uint:
+		return strconv.FormatUint(uint64(v), 10), nil
+	case uint8:
+		return strconv.FormatUint(uint64(v), 10), nil
+	case uint16:
+		return strconv.FormatUint(uint64(v), 10), nil
+	case uint32:
+		return strconv.FormatUint(uint64(v), 10), nil
+	case uint64:
+		return strconv.FormatUint(uint64(v), 10), nil
+	}
+	return "", fmt.Errorf("%v is not a string", i)
 }
