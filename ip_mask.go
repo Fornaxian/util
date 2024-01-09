e3fd5cc328f61bebd4c2d5a30f3c238c8c65a0df
Fornax <wim@fornaxian.tech>
2024-01-09 18:54:10 +0100

    Return net.IP from mask function

--- ip_mask.go
+++ ip_mask.go
@@ -6,12 +6,12 @@ import (
 
 // IPMaskPrefix applies a netmask to an IPv4 or IPv6 address. Only the bits
 // inside the mask are retained, the remaining bits are set to 0
-func IPMaskPrefix(addr net.IP, v4mask, v6mask int) (ipMask string) {
+func IPMask(addr net.IP, v4mask, v6mask int) net.IP {
 	if addr.To4() != nil {
 		// This is a IPv4 address, apply the v4 mask
-		return addr.Mask(net.CIDRMask(v4mask, 32)).String()
+		return addr.Mask(net.CIDRMask(v4mask, 32))
 	} else {
 		// This is a IPv6 address, apply the v6 mask
-		return addr.Mask(net.CIDRMask(v6mask, 128)).String()
+		return addr.Mask(net.CIDRMask(v6mask, 128))
 	}
 }
